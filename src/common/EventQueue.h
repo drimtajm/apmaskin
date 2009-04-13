a7dc0d67c10ad940e11bc097c5008f51eade5751
Börje Granberg <borje.granberg@cybercom.com>
2009-04-13 10:05:30 +0000

    Template classes can must have the full definition in the header file. Fixed EventQueue to do so.

--- src/common/EventQueue.h
+++ src/common/EventQueue.h
@@ -12,14 +12,45 @@
 #include <semaphore.h>
 #include <vector>
 
-class Event;
 template<class T>
 class EventQueue {
 public:
-	explicit EventQueue(int const bufferSize);
-	virtual ~EventQueue();
-	void send(T const * const event);
-	T const * receive();
+	explicit EventQueue(int const bufferSize) : size(bufferSize) {
+		eventBuffer = new T const *[bufferSize];
+		pthread_mutex_init(&mutex, NULL);
+		sem_init(&empty, 0, bufferSize);
+		sem_init(&full, 0, 0);
+		writeIndex = readIndex = 0;
+	};
+
+	virtual ~EventQueue() {
+		pthread_mutex_destroy(&mutex);
+		sem_destroy(&empty);
+		sem_destroy(&full);
+		while(!isEmpty())
+			delete receive();
+		delete [] eventBuffer;
+	};
+
+	void send(T const * const event) {
+		sem_wait(&empty);
+		pthread_mutex_lock(&mutex);
+		eventBuffer[writeIndex] = event;
+		writeIndex = (writeIndex + 1) % size;
+		pthread_mutex_unlock(&mutex);
+		sem_post(&full);
+	};
+
+	T const * receive() {
+		T const * e;
+		sem_wait(&full);
+		pthread_mutex_lock(&mutex);
+		e = eventBuffer[readIndex];
+		readIndex = (readIndex + 1) % size;
+		pthread_mutex_unlock(&mutex);
+		sem_post(&empty);
+		return e; //TODO: return event
+	};
 private:
 	bool isEmpty() const {return readIndex == writeIndex; };
 	pthread_mutex_t mutex;
