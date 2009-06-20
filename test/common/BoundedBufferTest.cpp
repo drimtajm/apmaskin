0c9a97ccb416e88a63ba62c0cf7255902496adde
Börje Granberg <borje.granberg@cybercom.com>
2009-06-20 15:30:04 +0000

    Added Google Test Framework to the project. :)

---
+++ test/common/BoundedBufferTest.cpp
@@ -0,0 +1,26 @@
+/*
+ * BoundedBufferTest.cpp
+ *
+ *  Created on: 2009-jun-19
+ *      Author: bege
+ */
+
+
+#include <test/gtest.h>
+#include "BoundedBuffer.h"
+
+class BoundedBufferTest : public testing::Test  {
+protected:
+	BoundedBufferTest() : buffer(4) {}
+	BoundedBuffer<int> buffer;
+
+};
+
+TEST_F(BoundedBufferTest, firstInShouldBeFirstOut) {
+  int a = 1;
+  int b = 2;
+  buffer.send(&a);
+  buffer.send(&b);
+  EXPECT_EQ(&a, buffer.receive());
+  EXPECT_EQ(&b, buffer.receive());
+}
