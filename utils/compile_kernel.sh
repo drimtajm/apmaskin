a3c54b30413aefa0e31226ef31e9a91f17040de1
Johan Kinnander <j.kinnander@gmail.com>
2012-06-17 14:38:45 +0200

    Added utils/install_apmaskin_debug.sh for installing apmaskin to filesystem/opt/apmaskin utils/compile_kernel.sh now creates filesystem/proc utils/compile_kernel.sh now copies bootloader to filesystem/boot Added file name patterns to .gitignore for files that should not be revision controlled Added "filesystem" to .gitignore Added "bootloader" to .gitignore (or should we git the bootloader binaries?) Added busybox and kernel paths to .gitignore

--- utils/compile_kernel.sh
+++ utils/compile_kernel.sh
@@ -4,4 +4,6 @@ make -j2 arch=arm CROSS_COMPILE=$COMPILER
 make -j2 modules arch=arm CROSS_COMPILE=$COMPILER
 sudo make CROSS_COMPILE=$COMPILER INSTALL_MOD_PATH=../filesystem arch=arm  modules_install
 mkdir -p ../filesystem/boot
+mkdir -p ../filesystem/proc
 sudo cp arch/arm/boot/zImage ../filesystem/boot/Image
+sudo cp ../bootloader/* ../filesystem/boot/
