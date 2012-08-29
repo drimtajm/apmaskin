28a47210e985a5a32de4ee4a9e1b015138e9ae69
Borje Granberg <borje.granberg@gmail.com>
2012-08-29 22:22:08 +0200

    Sync static files to target

--- utils/fs2usb.sh
+++ utils/fs2usb.sh
@@ -12,6 +12,7 @@ then
     sudo rsync -rav ${source}/* ${target}
     sudo rsync -rav /opt/crosstool/gcc-4.0.1-glibc-2.3.5/arm-unknown-linux-gnu/arm-unknown-linux-gnu/lib ${target}
     sudo rsync -rav /opt/crosstool/gcc-4.0.1-glibc-2.3.5/arm-unknown-linux-gnu/arm-unknown-linux-gnu/bin ${target}
+    sudo rsync -rav apmaskin/rootfs/* ${target}
 else
     echo ERROR! - ${target} is not a directory
 fi
