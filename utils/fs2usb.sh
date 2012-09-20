f6de10df0041f6006489dceb30b52021d18f4d6f
Borje Granberg <borje.granberg@gmail.com>
2012-09-20 20:10:06 +0200

    Start syslogd at startup
    create /dev/rfcomm when syncing to usb stick

--- utils/fs2usb.sh
+++ utils/fs2usb.sh
@@ -8,6 +8,7 @@ then
     sudo mknod ${target}/dev/console c 5 1 
     sudo mknod ${target}/dev/null c 1 3
     sudo mknod ${target}/dev/ttyAM0 c 204 14
+    sudo mknod ${target}/dev/rfcomm c 216 0
     sudo chown root.root ${source}/bin/busybox && sudo chmod +s ${source}/bin/busybox
     sudo rsync -rav ${source}/* ${target}
     sudo rsync -rav /opt/crosstool/gcc-4.0.1-glibc-2.3.5/arm-unknown-linux-gnu/arm-unknown-linux-gnu/lib ${target}
