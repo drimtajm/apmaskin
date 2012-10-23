06be0447043a80eddbce399d6530d3cb19775afe
Johan Kinnander <johan@marvin.(none)>
2012-10-23 20:29:40 +0200

    Changed utils/compile_kernel.sh to take target file system folder as parameter

--- utils/compile_kernel.sh
+++ utils/compile_kernel.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 COMPILER=arm-unknown-linux-gnu-
-make -j2 arch=arm CROSS_COMPILE=$COMPILER
-make -j2 modules arch=arm CROSS_COMPILE=$COMPILER
-sudo make CROSS_COMPILE=$COMPILER INSTALL_MOD_PATH=../filesystem arch=arm  modules_install
-mkdir -p ../filesystem/boot
-mkdir -p ../filesystem/proc
-sudo cp arch/arm/boot/zImage ../filesystem/boot/Image
-sudo cp ../bootloader/* ../filesystem/boot/
+
+if [ $# -eq 1 ] && [ -d $1 ]; then
+    make -j2 arch=arm CROSS_COMPILE=$COMPILER
+	make -j2 modules arch=arm CROSS_COMPILE=$COMPILER
+	sudo make CROSS_COMPILE=$COMPILER INSTALL_MOD_PATH=$1 arch=arm  modules_install
+	mkdir -p $1/proc
+	sudo cp arch/arm/boot/zImage $1/boot/Image
+else
+	echo "Error: Give target filesystem directory as first and only parameter"
+fi
