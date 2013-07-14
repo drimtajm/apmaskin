4a272b39456818a22f56d5abfb234a59aa6ac580
jokki <j.kinnander@gmail.com>
2013-07-14 11:41:48 +0200

    Restored debug prints

--- src/hello/HelloRaspberry.cpp
+++ src/hello/HelloRaspberry.cpp
@@ -6,10 +6,10 @@
 int main()
 {
   printf("Hello Raspberry!\n");
-//  DEBUG_PRINTF("This is a debug build\n");
-//  DEBUG_PRINTF("These are numbers: %d, %f, 0x%X\n", 4, 0.75, 0xFE);
-//  DEBUG_PRINTF("Line number: %d\n", __LINE__);
-//  DEBUG_PRINTF("Built at: %s, %s\n", __TIME__, __DATE__);
+  DEBUG_PRINTF("This is a debug build\n");
+  DEBUG_PRINTF("These are numbers: %d, %f, 0x%X\n", 4, 0.75, 0xFE);
+  DEBUG_PRINTF("Line number: %d\n", __LINE__);
+  DEBUG_PRINTF("Built at: %s, %s\n", __TIME__, __DATE__);
 //  Led::turnOnLed();
 //  Led::turnOffLed();
 //
