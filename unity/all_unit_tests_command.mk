33a3ad2be9e273e1f4598a9f31c481a165fcc7a0
jokki <j.kinnander@gmail.com>
2013-07-20 13:59:19 +0200

    Clarified "unity/all_unit_tests_command.mk"
    * Added file description
    * Added use of 'TEST_SUITE_SUFFIX' for test suite names

--- unity/all_unit_tests_command.mk
+++ unity/all_unit_tests_command.mk
@@ -1,3 +1,8 @@
+# "all_unit_tests_command.mk" defines a shell command for running all unit tests.
+# For this to work all test suites must be listed, space-separated, in the variable
+# 'UNIT_TEST_BINARIES' and have a name that ends with 'TEST_SUITE_SUFFIX' as defined in "makefile"
+
 SPACE     := $(empty) $(empty)
 SEMICOLON := ; 
-ALL_UNIT_TESTS_COMMAND = $(subst tests$(SPACE),tests$(SEMICOLON),$(UNIT_TEST_BINARIES))
+ALL_UNIT_TESTS_COMMAND = $(subst $(TEST_SUITE_SUFFIX)$(SPACE),$(TEST_SUITE_SUFFIX)$(SEMICOLON),\
+                         $(UNIT_TEST_BINARIES))
